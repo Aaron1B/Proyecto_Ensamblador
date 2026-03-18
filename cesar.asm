23c25dbcd7cd12b7506fcf4933043c10f914342d
Aarón Sacristán <aaronsacristan6a@gmail.com>
2026-03-18 17:21:27 +0100

    issue 3_2

--- cesar.asm
+++ cesar.asm
@@ -66,6 +66,18 @@ opcion_cifrar:
 
     mov [longitud_mensaje], eax ; Guardar Longitud
 
+    mov eax, 4                 
+    mov ebx, 1
+    mov ecx, pedir_clave
+    mov edx, pedir_clave_len
+    int 0x80
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, clave
+    mov edx, 2
+    int 0x80
+
     jmp salir
 
 opcion_descifrar:
