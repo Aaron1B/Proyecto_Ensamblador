ffc680d1672822c4d36c6f094a1ba48a53eab6cf
Aarón Sacristán <aaronsacristan6a@gmail.com>
2026-03-18 14:33:58 +0100

    Correcion_Issue_2

--- cesar.asm
+++ cesar.asm
@@ -1,43 +1,46 @@
 section .data
-    menu db "Menú", 10
-            "Cifrar", 10
-            "Descifrar", 10
-            "Salir", 10
+    menu db "Menú", 10, \
+            "Cifrar", 10, \
+            "Descifrar", 10, \
+            "Salir", 10, \
             "Selecciona una opción: "
     
-    menu_len equ $ - menu_msg
-
+    menu_len equ $ - menu
 
 section .bss
     opcion_usuario resb 1
-section .text
 
+section .text
 global _start
+
 _start:
     ; sys_write(stdout, menu, menu_len)
     mov  eax, 4        ; sys_write
     mov  ebx, 1        ; stdout
-    mov  ecx, menu     ; dirreccion del mensaje
+    mov  ecx, menu     ; direccion del mensaje
     mov  edx, menu_len ; longitud del mensaje
     int  0x80
+
     ; Leer opcion 
     mov eax,  3
     mov ebx,  0
-    mov ecx,  opcion
+    mov ecx,  opcion_usuario
     mov edx,  1 
     int 0x80
     
-    ;  Comparar opcion 
-    cmp byte [opcion], '1'
+    ; Comparar opcion 
+    cmp byte [opcion_usuario], '1'
     je  opcion_cifrar 
 
-    cmp byte [opcion], '2'
+    cmp byte [opcion_usuario], '2'
     je opcion_descifrar
 
-    cmp byte [opcion], '3'
+    cmp byte [opcion_usuario], '3'
     je salir
 
-    opcion_cifrar:
+    jmp _start
+
+opcion_cifrar:
     mov eax, 1
     mov ebx, 0
     int 0x80
@@ -47,8 +50,8 @@ opcion_descifrar:
     mov ebx, 0
     int 0x80
 
-    ;Salida del programa
+; Salida del programa
+salir:
     mov eax, 1
     mov ebx, 0
-    int 0x80 
-
+    int 0x80
